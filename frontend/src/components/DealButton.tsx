77c2ef490ce9c1f89ffd1933e5db91ceeed5cd7b
chwilldev <cedrichada@gmail.com>
2022-08-10 12:13:24 +0900

    removed: misuse of type

--- frontend/src/components/DealButton.tsx
+++ frontend/src/components/DealButton.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-type DealButtonProp = {
+type DealButtonProps = {
   handleDeal: () => void;
 };
 
-export const DealButton: React.FC<DealButtonProp> = ({ handleDeal }: DealButtonProp) => {
+export const DealButton: React.FC<DealButtonProps> = ({ handleDeal }) => {
   return (
     <button
       type="button"
-      className="bg-yellow-400 rounded-lg w-50 h-26 text-5xl font-bold py-3 px-6 hover:bg-yellow-600 hover:text-white"
+      className="bg-yellow-400 rounded-lg w-50 h-26 text-5xl font-bold py-3 px-6 hover:bg-yellow-600 hover:text-white font-alfa"
       onClick={handleDeal}
     >
       DEAL
